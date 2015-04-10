967a319eab2b61bd822fd04a825ea65c2a88fb44
Heng Li <lh3@me.com>
2015-04-10 13:47:16 -0400

    r296: a typo in command line help

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include "vcf.h"
 
-#define HTSBOX_VERSION "r295"
+#define HTSBOX_VERSION "r296"
 
 int main_samview(int argc, char *argv[]);
 int main_vcfview(int argc, char *argv[]);
